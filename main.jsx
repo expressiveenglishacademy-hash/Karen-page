f78de2ba307950a50f9aac947c6b6944efefcbbe
expressiveenglishacademy-hash <expressiveenglishacademy@gmail.com>
2026-04-21 17:55:03 -0700

    Delete main.jsx

--- main.jsx
+++
@@ -1,8 +0,0 @@
-window.KARENApp = window.KARENApp || {};
-
-(function renderKaren(app) {
-  const App = app.App;
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-
-  root.render(<App />);
-})(window.KARENApp);
